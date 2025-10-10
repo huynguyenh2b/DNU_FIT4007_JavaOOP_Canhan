2c0e9832bc077e1b31c60734b1a46d5e44d9d1f7
Nguyen Van Huy <2k4huynguyen37@gmail.com>
2025-10-10 11:47:07 +0700

    update

---
+++ CaNhan/src/SwitchCase/IdentifyDirection.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class IdentifyDirection {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a direction character (N, E, W, S): ");
+        // Read the first character of the input string and convert it to uppercase
+        char directionChar = scanner.next().toUpperCase().charAt(0);
+        String direction;
+
+        switch (directionChar) {
+            case 'N':
+                direction = "North";
+                break;
+            case 'E':
+                direction = "East";
+                break;
+            case 'W':
+                direction = "West";
+                break;
+            case 'S':
+                direction = "South";
+                break;
+            default:
+                direction = "Invalid direction character";
+                break;
+        }
+
+        System.out.println("Direction: " + direction);
+        scanner.close();
+    }
+}
---
+++ CaNhan/src/SwitchCase/ReadSingleDigitNumber.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class ReadSingleDigitNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a single digit number (0-9): ");
+        int number = scanner.nextInt();
+        String numberInVietnamese;
+
+        switch (number) {
+            case 0:
+                numberInVietnamese = "Không";
+                break;
+            case 1:
+                numberInVietnamese = "Một";
+                break;
+            case 2:
+                numberInVietnamese = "Hai";
+                break;
+            case 3:
+                numberInVietnamese = "Ba";
+                break;
+            case 4:
+                numberInVietnamese = "Bốn";
+                break;
+            case 5:
+                numberInVietnamese = "Năm";
+                break;
+            case 6:
+                numberInVietnamese = "Sáu";
+                break;
+            case 7:
+                numberInVietnamese = "Bảy";
+                break;
+            case 8:
+                numberInVietnamese = "Tám";
+                break;
+            case 9:
+                numberInVietnamese = "Chín";
+                break;
+            default:
+                numberInVietnamese = "Not a single digit number";
+                break;
+        }
+
+        System.out.println("Result: " + numberInVietnamese);
+        scanner.close();
+    }
+}
